b7794f4a0a42eca944340a1023e649135efd240e
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-05-06 17:51:26 +0100

    monitoring the app with logs

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
-const express = require('express')
-const { indexHandler, sayHelloHandler } = require('./routes/handlers')
-
+const express = require("express");
+const pino = require("pino");
+const { indexHandler, sayHelloHandler } = require("./routes/handlers");
 
 const app = express();
 const port = 3000;
 
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      translateTime: "SYS:dd-mm-yyyy HH:MM:ss", // formate the date and hour
+      ignore: "pino,hostname", // remove the machina name (marina-ubuntu)
+    },
+  },
+});
 
-app.get('/', indexHandler);
+app.get("/", (req, res) => {
+  logger.info("info: the index page has been visited");
+  logger.error();
+  ("error: the error message");
+  logger.warn("warn: the warn message");
+  logger.fatal("fatal: the fatal message");
+  // const currentTime = new Date();
+  // console.log("the index page has been visited");
+  res.send(
+    `Welcome to my app. Please use your the api/users endpoint to interact with users`
+  );
+});
 
-app.get('/hello/:name', sayHelloHandler);
+app.get("/hello/:name", sayHelloHandler);
 
 app.listen(port, () => {
-  console.log(`Hello world Application started and listening on http://localhost:${port}`)
-})
+  console.log(
+    `Hello world Application started and listening on http://localhost:${port}`
+  );
+});
