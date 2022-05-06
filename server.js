ceb1e3ae5b113f1260de37565a58aed418acb6e1
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-05-06 14:30:17 +0100

    Alierza bagheri- london8 - devops w3

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const { indexHandler, sayHelloHandler } = require('./routes/handlers')
+const express = require("express");
+const { indexHandler, sayHelloHandler } = require("./routes/handlers");
+const pino = require("pino");
+const logger = pino ({
+  transport:{
 
+    target:`pino-pretty`,
+options:{translateTime:true}
+  }
+})
 
 const app = express();
 const port = 3000;
 
+app.get("/", indexHandler);
 
-app.get('/', indexHandler);
-
-app.get('/hello/:name', sayHelloHandler);
-
+app.get("/hello/:name", sayHelloHandler);
+app.get("/api/", (req , res)=>{
+  logger.info(`the web page has been visited`);
+});
 
 
 app.listen(port, () => {
-  console.log(`Hello world Application started and listening on http://localhost:${port}`)
-})
+  console.log(
+    `Hello world Application started and listening on http://localhost:${port}`
+  );
+});
